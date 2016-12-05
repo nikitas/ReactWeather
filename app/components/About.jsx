cd382f2f2295d42bd72bae98cda42d733f88a442
Nikita T <nikita.s.tkachenko@gmail.com>
2016-12-05 12:39:24 +0200

    about page change

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,11 +1,5 @@
 var React = require('react');
 
-// var About = React.createClass({
-//     render: function () {
-
-//     }
-// });
-
 var About = (props) => {
     return (
         <h3>About Component</h3>
