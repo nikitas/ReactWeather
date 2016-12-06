4cb80b9114cbbb94f3c4fc89b78129444ed47433
Nikita T <nikita.s.tkachenko@gmail.com>
2016-12-06 11:07:28 +0200

    add error model

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -5,8 +5,14 @@ var About = (props) => {
         <div>
             <h2>Reactive weather </h2>
             <p>App that shows temperature in every city!</p>
-            <p>Weather information is gathered by Santa (Open weather map):</p>
-            <a href="http://openweathermap.org">Open weather map</a>
+            <p>Weather information is gathered by Santa <a href="http://openweathermap.org">(Open weather map)</a></p>
+            
+            <h3>Cool JS developers:</h3>
+            <ul className="no-bullet">
+                <li><a href="https://www.linkedin.com/in/nikitat">Nikita Tkachenko</a></li>
+                <li><a href="https://ua.linkedin.com/in/oleksandr-nechai-7948918">Sasha Nechai</a></li>
+            </ul>
+
         </div>
 
     )
